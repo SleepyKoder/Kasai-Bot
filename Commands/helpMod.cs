97603a78915514444417e2d5dd69ff239015d26e
scovee <30730053+scovee@users.noreply.github.com>
2018-07-28 14:15:45 +0800

    added help command

---
+++ Commands/helpMod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace Kasaibot.Commands
+{
+    public class helpMod : ModuleBase<SocketCommandContext>
+    {
+        EmbedBuilder Embed = new EmbedBuilder();
+        [Command("help")]
+        public async Task help([Remainder]string message)
+        {
+            Embed.WithAuthor("~Kasai~");
+            Embed.WithColor(new Color(127, 255, 0));
+            Embed.WithDescription("Visit the link for the bot's documentation and all the help you would ever need");
+            Embed.WithUrl("https://github.com/SleepyKoder/Kasai-Bot");
+        }
+    }
+}
